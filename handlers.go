6c056152c9b9134e2a11b1f9876c46acf7a59b9b
Faisal Ali <fai28683@gmail.com>
2020-08-21 12:22:54 +0100

    fix typo's & colour correction

--- handlers.go
+++ handlers.go
@@ -13,13 +13,13 @@ func homeHandler(c echo.Context) error {
 
 // Handler: Second | Handler send to second page
 func secondHandler(c echo.Context) error {
-	Debugf("Publishing the home page")
+	Debugf("Publishing the second page")
 	return c.Render(http.StatusUnauthorized, "second.html", "")
 }
 
 // Handler: Api | Handler that serves the API data
 func apiHandler(c echo.Context) error {
-	Debugf("Publishing the home page")
+	Debugf("Publishing the api")
 	return c.JSON(http.StatusOK, map[string]string{
 		`key1`: `value1`,
 		`key2`: `value2`,
